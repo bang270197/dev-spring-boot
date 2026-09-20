bfbdbb8aebc69f94cedbc11a59b9bc6ecff6273c
nguyenngocbang <bang270197@gmail.com>
2026-09-20 10:10:16 +0700

    add save user in AUTH_WHITELIST

--- src/main/java/com/devteria/springboot/configuration/SecurityConfig.java
+++ src/main/java/com/devteria/springboot/configuration/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     private static final String[] AUTH_WHITELIST =
             {       "/api/v1/auth/introspect",
                     "/api/v1/auth/login",
+                    "/api/v1/users"
             };
 
     @Bean
